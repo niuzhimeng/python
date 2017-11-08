86f904d11e710e35a99cebc227f9d90665d197e2
niuzhimeng <295290968@qq.com>
2017-11-08 18:21:52 +0800

    优化看笑话体验

--- test/ioTest/NoGetSistert.py
+++ test/ioTest/NoGetSistert.py
@@ -1,12 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.session().get('http://www.budejie.com/text/13', headers={'User-Agent':
-                                                              'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'})
 
-pageInfo = BeautifulSoup(page.text, 'html.parser')
+def get():
+    page = requests.session().get('http://www.budejie.com/text/15', headers={'User-Agent':
+                                                                                 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'})
 
-div = pageInfo.find_all('div', 'j-r-list-c-desc')
+    pageInfo = BeautifulSoup(page.text, 'html.parser')
 
-for b in div:
-    print(str(b.a) + '\r\n')
+    div = pageInfo.find_all('div', 'j-r-list-c-desc')
+
+    for i in range(0, div.__len__()):
+        s = str(div[i].a.get_text())
+        if s.__len__() > 70:
+            print('(' + str(i + 1) + ') ' + huanhang(s))
+
+        else:
+            print('(' + str(i + 1) + ') ' + s + '\r\n')
+
+
+def huanhang(temp):
+    text = ''
+    while temp:
+        text = text + temp[:78] + "\r\n"
+        temp = temp[78:]
+    return text
+
+
+if __name__ == '__main__':
+    get()
--- test1/web.py
+++ test1/web.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask, jsonify  # 导入Flask类
 from flask import request
-
+import time
 app = Flask(__name__)  # 创建Flask类的一个实例
 
 
@@ -10,7 +10,8 @@ app = Flask(__name__)  # 创建Flask类的一个实例
 def index():
     phone = request.args.get('phone')
     url = 'http://www.ip138.com:8080/search.asp?mobile=' + phone + '&action=mobile'
-    page = requests.session().get(url)
+    page = requests.get(url)
+    time.sleep(0.3)
     page.encoding = 'gbk'
     info = BeautifulSoup(page.text, 'lxml')
     pageInfo = info.find_all('td', attrs={'align': 'center'})
