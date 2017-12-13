d4fbca650103e47a082ee46f696692ae81f15d03
niuzhimeng <295290968@qq.com>
2017-12-13 17:40:17 +0800

    food plus

--- traffic/shipin.py
+++ traffic/shipin.py
@@ -1,9 +1,10 @@
-import requests
-from flask import json
 import uuid
 
 import pymysql
+import requests
+from flask import json
 
+session = requests.session()
 connection = pymysql.connect(host='localhost',
                              port=3306,
                              user='root',
@@ -11,7 +12,8 @@ connection = pymysql.connect(host='localhost',
                              db='amdb',
                              charset='utf8')
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
 }
 try:
     for index in range(1, 271):
@@ -19,8 +21,8 @@ try:
         data = {
             'page': index
         }
-        html = requests.post('http://125.35.6.84:81/xk/itownet/portalAction.do?method=getXkzsList', headers=headers,
-                             data=data)
+        html = session.post('http://125.35.6.84:81/xk/itownet/portalAction.do?method=getXkzsList', headers=headers,
+                            data=data)
         res = json.loads(html.text)
         list = res['list']
         for i in range(int(len(list))):
@@ -30,10 +32,31 @@ try:
             validity_date = list[i]['XK_DATE']  # 有效期
             send_paper_time = list[i]['XC_DATE']  # 发证期
             # 获得数据库游标
+            data1 = {
+                'id': list[i]['ID']
+            }
+            response = session.post('http://125.35.6.84:81/xk/itownet/portalAction.do?method=getXkzsById', data=data1,
+                                    headers=headers)
+
+            res1 = json.loads(response.text)
+            # 点击后获得的内容
+            certStr = res1['certStr']  # 许可项目
+            epsAddress = res1['epsAddress']  # 企业住所
+            epsProductAddress = res1['epsProductAddress']  # 生产地址
+            businessLicenseNumber = res1['businessLicenseNumber']  # 社会信用代码
+            legalPerson = res1['legalPerson']  # 法定代表人
+            businessPerson = res1['businessPerson']  # 企业负责人
+            qualityPerson = res1['qualityPerson']  # 质量负责人
+            rcManagerDepartName = res1['rcManagerDepartName']  # 日常监督管理机构
+            rcManagerUser = res1['rcManagerUser']  # 日常监督管理人员
+            xkName = res1['xkName']  # 签发人
             with connection.cursor() as cursor:
-                sql = 'insert into food(id, company_name, licence, send_paper, validity_date, send_paper_time)' \
-                      ' values(%s, %s, %s, %s,%s, %s)'
-                cursor.execute(sql, (int(uuid.uuid1()), name, licence, send_paper, validity_date, send_paper_time))
+                sql = 'insert into food(id, company_name, licence, send_paper, validity_date, send_paper_time,' \
+                      'certStr, epsAddress, epsProductAddress, businessLicenseNumber, legalPerson, businessPerson, qualityPerson, rcManagerDepartName, rcManagerUser,xkName)' \
+                      ' values(%s, %s, %s, %s,%s, %s,%s, %s, %s, %s,%s, %s,%s, %s, %s, %s)'
+                cursor.execute(sql, (int(uuid.uuid1()), name, licence, send_paper, validity_date, send_paper_time,
+                                     certStr, epsAddress, epsProductAddress, businessLicenseNumber, legalPerson,
+                                     businessPerson, qualityPerson, rcManagerDepartName, rcManagerUser,xkName))
         # 事务提交
         connection.commit()
 finally:
--- traffic/ttt.py
+++ traffic/ttt.py
@@ -1,2 +1,8 @@
-for i in range(1, 3):
-    print(i)
+import requests
+
+data = {
+    'id': '86b60fb7df4e4be9bac1350557227352'
+}
+response = requests.post('http://125.35.6.84:81/xk/itownet/portalAction.do?method=getXkzsById', data=data)
+html = response.text
+print(html)
